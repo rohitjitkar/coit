becf5632fd6da68aad1965dd38b23af24488d775
rohitjitkar <103124977+rohitjitkar@users.noreply.github.com>
2023-10-16 19:11:12 +0530

    Update App.js

--- coit-frontend/src/App.js
+++ coit-frontend/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        fetch('http://52.66.121.211:8080/sentiment', {
+        fetch('http://localhost:8080/sentiment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
